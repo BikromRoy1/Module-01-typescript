3dfdbb21c368f06c6a6908babb700fb9d6174c76
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-12-24 18:27:17 +0600

    Getters and setters

--- src/Module-04/Inheritence.ts
+++ src/Module-04/Inheritence.ts
@@ -1,50 +1,51 @@
-class Parent {
-  name: string;
-  age: number;
-  address: string;
+// class Parent {
+//   name: string;
+//   age: number;
+//   address: string;
 
-  constructor(name: string, age: number, address: string) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
-  }
-  makeSleep(hours: number): string {
-    return `this ${this.name} will sleep for ${hours}`;
-  }
-}
+//   constructor(name: string, age: number, address: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+//   }
+//   makeSleep(hours: number): string {
+//     return `this ${this.name} will sleep for ${hours}`;
+//   }
+// }
 
-class Student extends Parent {
-  // Inheritance for parent class
-  //   name: string;
-  //   age: number;
-  //   address: string;
-  constructor(name: string, age: number, address: string) {
-    super(name, age, address);
-    // this.name = name;
-    // this.age = age;
-    // this.address = address;
-  }
-  //   makeSleep(hours: number): string {
-  //     return `this ${this.name} will sleep for ${hours}`;
-  //   }
-}
+// class Student extends Parent {
+//   // Inheritance for parent class
+//   //   name: string;
+//   //   age: number;
+//   //   address: string;
+//   constructor(name: string, age: number, address: string) {
+//     super(name, age, address);
+//     // this.name = name;
+//     // this.age = age;
+//     // this.address = address;
+//   }
+//   //   makeSleep(hours: number): string {
+//   //     return `this ${this.name} will sleep for ${hours}`;
+//   //   }
+// }
 
-const student1 = new Student('Komi', 30, 'united states');
-const result6 = student1.makeSleep(5);
+// const student1 = new Student('Komi', 30, 'united states');
+// const result6 = student1.makeSleep(5);
 
-console.log(result6);
+// console.log(result6);
 
-class Teacher extends Parent {
-  designation: string;
+// class Teacher extends Parent {
+  
+//   designation: string;
 
-  constructor(name: string, age: number, address: string, designation: string) {
-    super(name, age, address);
-    this.designation = designation;
-  }
+//   constructor(name: string, age: number, address: string, designation: string) {
+//     super(name, age, address);
+//     this.designation = designation;
+//   }
 
-  takeClass(numberOfClass: number): string {
-    return `This ${this.name} will take ${numberOfClass} class`;
-  }
-}
+//   takeClass(numberOfClass: number): string {
+//     return `This ${this.name} will take ${numberOfClass} class`;
+//   }
+// }
 
-const Teacher1 = new Teacher('sunny leone', 55, 'India', 'Teaching');
+// const Teacher1 = new Teacher('sunny leone', 55, 'India', 'Teaching');
---
+++ src/Module-04/Modifiers.ts
@@ -0,0 +1,21 @@
+// class BankAccount {
+//   public id: number;
+//   public name: string;
+//   protected balance: number;
+
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this.balance = balance;
+//   }
+// }
+
+// class StudentAccount extends BankAccount {
+//     test() {
+//         this.balance
+//     }
+// }
+
+// const myAccount = new BankAccount(522, 'Bikrom', 60000);
+// myAccount.balance = 0;
+// console.log(myAccount);
